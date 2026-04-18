9c5e9fe3fb79b1be30bcc6ffc4c8667c53d5a9b5
Khairul Rajib <khairulrajib99@gmail.com>
2026-04-18 08:35:12 +0700

    Add files via upload

---
+++ P5/latihan1.java
@@ -0,0 +1,77 @@
+
+import java.util.Scanner;
+
+
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author DELL
+ */
+
+       import java.util.Scanner;
+
+public class latihan1{
+
+public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+
+    int kehadiran;
+    int total_nilai;
+    int validasi;
+    String grade;
+
+    System.out.print("Masukan Jumlah Kehadiran Dalam Satu Semester: ");
+    kehadiran = sc.nextInt();
+
+    System.out.print("Masukan Total Nilai: ");
+    total_nilai = sc.nextInt();
+
+    //percabangan untuk menentukan kondisi nilai
+    if(total_nilai >= 80){
+      
+      grade = "A";
+
+    } else if(total_nilai >= 70){
+
+      grade = "B";
+
+    } else if(total_nilai >= 60){
+
+      grade = "C";
+      
+    } else if(total_nilai >= 55){
+
+      grade = "D";
+
+    }else{
+      
+      grade = "E";
+
+    }
+
+    //proses validasi
+    validasi = 21 * 75/100;
+
+    if(kehadiran >= validasi){
+      System.out.println("Nilai: " + total_nilai);
+      System.out.println("Grade: " + grade);
+    } else{
+      if(total_nilai >= 55){
+        total_nilai = 55;
+        grade = "D";
+        System.out.println("Nilai: " + total_nilai);
+        System.out.println("Grade: " + grade);
+      } else{
+        System.out.println("Nilai: " + total_nilai);
+        System.out.println("Grade: E");
+      }
+
+    } 
+    
+  }
+}
